a6aea3c6b0cd013b196aefd034c7ad4a9a6f2a8f
tema-liu <vickyliu22205960@gmail.com>
2024-11-17 23:28:08 +0800

    新增-預設ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import theme from "./styles/theme.ts";
 import App from "./App.tsx";
 import GlobalStyle from "./GlobalStyle.tsx";
+import { ThemeProvider } from "styled-components";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+const colors = {
+  light: "#FFFFFF",
+  dark: "#000000",
+  primary: "#ED008C",
+  container1: "#FF6063",
+  container2: "#FF983E",
+  container3: "#3EDF77",
+  container4: "#85BDFD",
+  outline1: "#FF7ECB",
+  outline2: "#FFC003",
+  outline3: "#7AFA7A",
+  outline4: "#7AFAF6",
+  gray100: "#FAFAFA",
+  gray200: "#F4F4F4",
+  gray300: "#ECECEC",
+  gray400: "#D4D4D4",
+  gray600: "#7F7F7F",
+  gray700: "#515151",
+  gray800: "#353434",
+  gray900: "#212121",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
