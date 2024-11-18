e5141c1985743ae5d7dbc32b2681c5b8989f0ff7
tema-liu <vickyliu22205960@gmail.com>
2024-11-18 22:23:31 +0800

    新增-設定global字體大小

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -22,7 +22,24 @@ a,button{
     cursor: pointer;
 }
 
-
+h1{
+    font-size: 20px;
+}
+h2{
+    font-size: 17px;
+}
+h3{
+    font-size: 15px;
+}
+h4{
+    font-size: 14px;
+}
+h5{
+    font-size: 12px;
+}
+h6{
+    font-size: 11px;
+}
 
 `;
 
