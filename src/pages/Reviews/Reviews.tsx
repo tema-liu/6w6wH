4ade3f6780238e562f69d5bc3a3df7b53765b958
tema-liu <vickyliu22205960@gmail.com>
2024-11-23 02:20:55 +0800

    新增-評論卡片完成

--- src/pages/Reviews/Reviews.tsx
+++ src/pages/Reviews/Reviews.tsx
@@ -1,14 +1,34 @@
 import Header from "../../component/header";
+import badge from "../../assets/badge.png";
+import badge2 from "../../assets/badge2.png";
+import overflowIcon from "../../assets/overflow.png";
+import headShotIcon from "../../assets/4d7a9ac84094d8ed9c205d7b69288815.jpg";
+import { StarRating } from "../../component/StarRating";
 import leftBtn from "../../assets/Frame.png";
 import rightBtn from "../../assets/Vector.png";
 import { Wrapper, Container, IconImg } from "../../component/LayoutComponents";
 import {
+  UserReviewFooter,
+  Icon,
   ImageSection,
   StoreImg,
   BtnContainer,
   CarouselBtn,
+  CommentCardContent,
+  CommentCardDetail,
+  Head,
+  HeadRight,
+  BadgeBox,
+  UserRating,
+  UserReviewTop,
+  UserReviewMain,
+  HeadShot,
+  SocialBlock,
 } from "../Reviews/styled";
 import FooterNav from "../../component/FooterNav";
+import heart from "../../assets/heart.png";
+import review from "../../assets/review.png";
+import shareIcon from "../../assets/share.png";
 
 function Reviews() {
   return (
@@ -27,6 +47,48 @@ function Reviews() {
               </CarouselBtn>
             </BtnContainer>
           </ImageSection>
+          <CommentCardContent>
+            <CommentCardDetail>
+              <Head>
+                <HeadShot src={headShotIcon} alt="headShot" />
+                <BadgeBox>
+                  <img width={22} src={badge} alt="badge" />
+                  <img width={22} src={badge2} alt="badge2" />
+                </BadgeBox>
+              </Head>
+              <HeadRight>
+                <UserReviewTop>
+                  <UserRating>
+                    <span style={{ display: "block" }}>Ala</span>
+                    <StarRating star={3} width={112} height={16} />
+                  </UserRating>
+                  <div>
+                    <Icon src={shareIcon} alt="share" />
+                    <Icon src={overflowIcon} alt="overflow" />
+                  </div>
+                </UserReviewTop>
+                <UserReviewMain>
+                  <p>
+                    Kopi susu is super yummy! Nice ambient and service! Come
+                    hang out!
+                  </p>
+                </UserReviewMain>
+                <UserReviewFooter>
+                  <h5>3 hour ago</h5>
+                  <SocialBlock>
+                    <div>
+                      <h4>1.5k</h4>
+                      <IconImg src={review} alt="review" />
+                    </div>
+                    <div>
+                      <h4>999</h4>
+                      <IconImg src={heart} alt="heart" />
+                    </div>
+                  </SocialBlock>
+                </UserReviewFooter>
+              </HeadRight>
+            </CommentCardDetail>
+          </CommentCardContent>
         </Container>
         <FooterNav />
       </Wrapper>
--- src/pages/Reviews/styled.ts
+++ src/pages/Reviews/styled.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { IconImg } from "../../component/LayoutComponents";
 
 export const ImageSection = styled.div`
   position: relative;
@@ -28,4 +29,75 @@ export const CarouselBtn = styled.button`
   &:hover {
     opacity: 1;
   }
+`;
+
+export const CommentCardContent = styled.div`
+  padding: 16px 8px 16px 8px;
+  border-radius: 0 0 32px 32px;
+  background-color: ${({ theme }) => theme.colors.gray100};
+`;
+
+export const CommentCardDetail = styled.div`
+  display: flex;
+  margin-top: 8px;
+`;
+
+export const Head = styled.div`
+  width: 64px;
+`;
+
+export const HeadRight = styled.div`
+  width: 100%;
+  margin-left: 8px;
+`;
+export const HeadShot = styled.img`
+  width: 64px; /* 設置圖片寬度 */
+  height: 64px; /* 設置圖片高度（確保是正方形） */
+  border-radius: 50%; /* 圓形裁切 */
+  object-fit: cover; /* 確保圖片內容不變形 */
+`;
+export const BadgeBox = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 8px;
+`;
+export const UserReviewTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserRating = styled.div`
+  * + * {
+    margin-top: 8px; // 子元素之間的間距
+  }
+`;
+export const Icon = styled(IconImg)`
+  margin: 8px;
+`;
+
+export const UserReviewMain = styled.div`
+  padding: 4px 0 8px 0;
+`;
+
+export const SocialBlock = styled.div`
+  display: flex;
+  div {
+    width: 88px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    img {
+      margin-left: 4px;
+    }
+  }
+`;
+
+export const UserReviewFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 12px;
+  margin-bottom: 12px;
+  color: ${({ theme }) => theme.colors.gray600};
 `;
