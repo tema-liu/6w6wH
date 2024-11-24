eb93b6cab7b38db291a0573e44fc98b903a6adc1
tema-liu <vickyliu22205960@gmail.com>
2024-11-25 02:29:44 +0800

    新增-ChipGroup區塊完成

---
+++ src/component/TagChips.tsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import closeIcon from "../assets/close.png";
+
+const Content = styled.p`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+const TagChip = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  background: ${({ theme }) => theme.colors.light};
+  max-width: 100%;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 22px;
+  letter-spacing: -0.41px;
+  box-shadow: 0px 0px 4px 0px #00000033, 0px 0px 8px 0px #0000001a;
+
+  // 左側紅色條
+  &::before {
+    flex-shrink: 0;
+    content: "";
+    width: 8px;
+    height: 100%;
+    background: ${({ theme }) => theme.colors.container1};
+    margin-right: 8px;
+  }
+`;
+
+const CloseButton = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background: none;
+  flex-shrink: 0;
+  padding: 16px 16px 16px 8px;
+  cursor: pointer;
+  img {
+    width: 14px;
+    height: 14px;
+  }
+`;
+
+interface TagChipProps {
+  label: string;
+  onRemove?: () => void;
+}
+
+export const TagChips = ({ label, onRemove }: TagChipProps) => (
+  <TagChip>
+    <Content>{label}</Content>
+    <CloseButton onClick={onRemove}>
+      <img
+        src={closeIcon}
+        onClick={() => {
+          console.log("刪除");
+        }}
+        alt="close"
+      />
+    </CloseButton>
+  </TagChip>
+);
--- src/pages/SearchResult/SearchResult.tsx
+++ src/pages/SearchResult/SearchResult.tsx
@@ -1,10 +1,59 @@
 import styled from "styled-components";
 import Header from "../../component/header";
+import { Container, Wrapper } from "../../component/LayoutComponents";
+import FooterNav from "../../component/FooterNav";
+import bugIcon from "../../assets/bug.png";
+import { TagChips } from "../../component/TagChips";
+const ChipGroup = styled.div``;
+const TitleBox = styled.div`
+  border-radius: 16px 16px 0 0;
+  background-color: ${({ theme }) => theme.colors.gray100};
+  width: fit-content;
+  padding: 2px 16px 0 16px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray400};
+`;
+const TitleBoxIcon = styled.img`
+  width: 32px;
+  height: auto;
+`;
+const TitleBoxText = styled.h2`
+  margin-left: 8px;
+  display: inline-block;
+`;
+const TagBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: end;
+  padding: 16px;
+  border-radius: 0 16px 16px 16px;
+  background-color: ${({ theme }) => theme.colors.gray100};
+  gap: 8px;
+`;
 
 function SearchResult() {
   const num = 5;
 
-  return <Header title={`SearchResult(${num})`} />;
+  return (
+    <>
+      <Wrapper>
+        <Header title={`SearchResults(${num})`} />
+        <Container>
+          <ChipGroup>
+            <TitleBox>
+              <TitleBoxIcon src={bugIcon} alt="bugIcon" />
+              <TitleBoxText>R11 Kaohsiung Main Station</TitleBoxText>
+            </TitleBox>
+            <TagBox>
+              <TagChips label={"food"} />
+              <TagChips label={"Multilingual"} />
+              <TagChips label={"Friendly"} />
+            </TagBox>
+          </ChipGroup>
+        </Container>
+        <FooterNav />
+      </Wrapper>
+    </>
+  );
 }
 
 export default SearchResult;
