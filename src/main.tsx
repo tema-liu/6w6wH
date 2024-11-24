e26cd9f6534540c8ac234d87debfd420aeefe8b2
tema-liu <vickyliu22205960@gmail.com>
2024-11-24 22:26:03 +0800

    新增-建置SearchResult頁面含header切版

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,13 @@ import GlobalStyle from "./GlobalStyle.tsx";
 import { ThemeProvider } from "styled-components";
 import StoreDetail from "./pages/StoreDetail/StoreDetail.tsx";
 import Reviews from "./pages/Reviews/Reviews.tsx";
+import SearchResult from "./pages/SearchResult/SearchResult.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
-      <StoreDetail />
+      <SearchResult />
     </ThemeProvider>
   </StrictMode>
 );
---
+++ src/pages/SearchResult/SearchResult.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import Header from "../../component/header";
+
+function SearchResult() {
+  const num = 5;
+
+  return <Header title={`SearchResult(${num})`} />;
+}
+
+export default SearchResult;
