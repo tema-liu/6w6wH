0259a21bc4e0c37842e70de815ec66e0630d2b75
tema-liu <vickyliu22205960@gmail.com>
2024-11-27 01:26:46 +0800

    新增-搜尋按鈕以及車站按鈕完成

--- src/pages/Search/search.tsx
+++ src/pages/Search/search.tsx
@@ -1,32 +1,75 @@
 import { Wrapper, Container, IconImg } from "../../component/LayoutComponents";
 import Header from "../../component/header";
 import FooterNav from "../../component/FooterNav";
-import styled from "styled-components";
 import bugIcon from "../../assets/bug.png";
+import { TitleBoxIcon, TitleBoxText } from "../../component/TitleBox";
 import {
-  TitleBox,
-  TitleBoxIcon,
-  TitleBoxText,
-  TagBox,
-} from "../../component/TitleBox";
+  SuggestBtn,
+  SearchContainer,
+  StyledTagBox,
+  StyledTitleBox,
+  SegmentedControlInner,
+  RadioInput,
+  Label,
+} from "./styled";
 import CheckBox from "./CheckBox";
 import { useState } from "react";
+import searchIcon from "../../assets/search.png";
+import styled from "styled-components";
+import addIcon from "../../assets/addIcon.png";
 
-const StyledTitleBox = styled(TitleBox)`
-  box-shadow: 0px 4px 16px 4px #0000000a, 0px 2px 8px 0px #0000001a;
+const StationList = styled.div`
+  position: relative;
+  background-color: ${({ theme }) => theme.colors.gray100};
+  border-radius: 0 0 16px 16px;
+  padding: 16px 16px 16px 42px;
+`;
+const MoonBtn = styled.button`
+  padding: 12px 16px;
+  max-width: 100%;
+  border: 0;
+  background-color: ${({ theme }) => theme.colors.light};
+  box-shadow: 8px 0px 0px #ff6063 inset, 0px 0px 4px 0px #00000033,
+    0px 0px 8px 0px #0000001a;
+
+  border-radius: 20px;
+  font-weight: 700;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
-const StyledTagBox = styled(TagBox)`
-  box-shadow: 0px 4px 16px 4px #0000000a, 0px 2px 8px 0px #0000001a;
-`;
-
-const SearchContainer = styled.form`
-  display: flex;
-  flex-direction: column;
-  row-gap: 24px;
+const PlusIcon = styled.div`
+  display: inline-block;
+  position: absolute;
+  bottom: 0px;
+  left: 0;
+  font-size: 0;
+  /* 扇形 */
+  &::before {
+    content: "";
+    width: 42px;
+    height: 42px;
+    background: ${({ theme }) => theme.colors.gray400};
+    border-radius: 0 0 0 16px; /* 左下圓角 */
+    clip-path: circle(100% at 0% 100%); /* 創建扇形形狀 */
+    display: inline-block;
+  }
+  img {
+    position: absolute;
+    bottom: 11px;
+    left: 11px;
+  }
 `;
 
 function Search() {
+  const [selectedOption, setSelectedOption] = useState("HSR");
+
+  // 處理選擇的變更
+  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(event.target.value);
+  };
+
   type CheckedState = {
     [key: string]: boolean;
   };
@@ -58,7 +101,12 @@ function Search() {
     <Wrapper>
       <Header title={"Search"} />
       <Container>
-        <SearchContainer>
+        <SearchContainer
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log(category, friendly);
+          }}
+        >
           <div>
             <StyledTitleBox>
               <TitleBoxIcon src={bugIcon} alt="bugIcon" />
@@ -97,6 +145,65 @@ function Search() {
               ))}
             </StyledTagBox>
           </div>
+          <div
+            style={{
+              filter:
+                "drop-shadow(0px 4px 16px #0000000a) drop-shadow(0px 2px 8px #0000001a)",
+            }}
+          >
+            <SegmentedControlInner>
+              <RadioInput
+                id="Location"
+                type="radio"
+                value="Location"
+                checked={selectedOption === "Location"}
+                onChange={handleOptionChange}
+              />
+              <Label htmlFor="Location">Location</Label>
+              <RadioInput
+                id="HSR"
+                type="radio"
+                value="HSR"
+                checked={selectedOption === "HSR"}
+                onChange={handleOptionChange}
+              />
+              <Label htmlFor="HSR">HSR</Label>
+              <RadioInput
+                id="Train"
+                type="radio"
+                value="Train"
+                checked={selectedOption === "Train"}
+                onChange={handleOptionChange}
+              />
+              <Label htmlFor="Train">Train</Label>
+              <RadioInput
+                id="LRT"
+                type="radio"
+                value="LRT"
+                checked={selectedOption === "LRT"}
+                onChange={handleOptionChange}
+              />
+              <Label htmlFor="LRT">LRT</Label>
+              <RadioInput
+                id="MRT"
+                type="radio"
+                value="MRT"
+                checked={selectedOption === "MRT"}
+                onChange={handleOptionChange}
+              />
+              <Label htmlFor="MRT">MRT</Label>
+            </SegmentedControlInner>
+            <StationList>
+              <MoonBtn>R11 Kaohsiung Main Station</MoonBtn>
+              <PlusIcon>
+                <img src={addIcon} alt="addIcon" />
+              </PlusIcon>
+            </StationList>
+          </div>
+          <SuggestBtn>
+            <IconImg src={searchIcon} alt="penIcon" />
+            Search
+          </SuggestBtn>
         </SearchContainer>
       </Container>
       <FooterNav />
---
+++ src/pages/Search/styled.ts
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+import { TitleBox, TagBox } from "../../component/TitleBox";
+
+export const StyledTitleBox = styled(TitleBox)`
+  box-shadow: 0px 4px 16px 4px #0000000a, 0px 2px 8px 0px #0000001a;
+`;
+
+export const StyledTagBox = styled(TagBox)`
+  box-shadow: 0px 4px 16px 4px #0000000a, 0px 2px 8px 0px #0000001a;
+`;
+
+export const SearchContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
+`;
+
+export const SuggestBtn = styled.button`
+  width: 100%;
+  column-gap: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.gray100};
+  border-radius: 16px;
+  padding: 13px 0;
+  margin: 16px 0;
+  box-shadow: 0px 0px 4px 0px #00000033, 0px 0px 8px 0px #0000001a;
+`;
+
+export const SegmentedControlInner = styled.div`
+  display: flex;
+`;
+
+export const RadioInput = styled.input`
+  clip: rect(0, 0, 0, 0);
+  opacity: 0;
+  position: absolute;
+`;
+
+export const Label = styled.label`
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-block;
+  flex: 1;
+  font-size: 17px;
+  line-height: 22px;
+  padding: 12px 0;
+  letter-spacing: -0.41px;
+  font-weight: 700;
+  text-align: center;
+  transition: opacity 0.4s ease;
+  background: ${({ theme }) => theme.colors.gray100};
+  opacity: 0.5;
+  color: ${({ theme }) => theme.colors.gray900};
+
+  /* First label */
+  &:first-of-type {
+    border-radius: 16px 16px 0 0;
+    border-right: none;
+    margin-right: 8px;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  &:nth-of-type(2) {
+    border-radius: 16px 0 0 0;
+    border-right: none;
+  }
+
+  /* Last label */
+  &:last-of-type {
+    border-radius: 0 32px 0 0;
+  }
+
+  /* Checked state */
+  ${RadioInput}:checked + & {
+    background: ${({ theme }) => theme.colors.gray100};
+    border-bottom: 1px solid ${({ theme }) => theme.colors.gray400};
+    opacity: 1;
+  }
+
+  /* Focus state */
+  ${RadioInput}:focus + & {
+    background: ${({ theme }) => theme.colors.gray100};
+    opacity: 1;
+  }
+`;
