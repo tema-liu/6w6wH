3ae561234920ff160cd35efc600da23667749712
tema-liu <vickyliu22205960@gmail.com>
2024-12-02 13:23:19 +0800

    樣式-修正layout -修正footer及outlet布局

--- src/StoreListLayout.tsx
+++ src/StoreListLayout.tsx
@@ -3,10 +3,10 @@ import FooterNav from "./component/FooterNav";
 
 function StoreListLayout() {
   return (
-    <div>
+    <>
       <Outlet /> {/* 用於渲染子路由內容 */}
       <FooterNav />
-    </div>
+    </>
   );
 }
 
