f1a4bc03aa263cdef166f1b3c9614877dd84863e
tema-liu <vickyliu22205960@gmail.com>
2024-12-06 03:37:25 +0800

    新增-Review型別增加 -增加userPhoto欄位 -tag型別修改

--- src/type/type.ts
+++ src/type/type.ts
@@ -3,6 +3,7 @@ type Reply = {
   replyID: string; // 留言ID
   userID: string; // 留言人ID
   userName: string; // 留言人名稱
+  userPhoto: string;
   comment: string; // 留言內容
   postedAt: Date; // 發布時間
   medal: string | null; // 獎牌
@@ -14,12 +15,14 @@ export type Comment = {
   commentID: string; // 評論ID
   userID: string; // 評論人ID
   userName: string; // 評論人名稱
+  userPhoto: string;
+  photo: string[]; //照片
   starCount: number; // 星星數量
   comment: string | null; // 評論內容
   postedAt: Date; // 發布時間
   likeCount: number; // 愛心數
   isLike: boolean; // 是否已按愛心
-  tag: [string, string, string]; // 評價標籤
+  tag: string[]; // 評價標籤
   medal: string | null; // 獎牌
   reply: null | Reply[]; // 留言回覆
 };
