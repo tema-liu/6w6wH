a9d5deb0f81f7cbe20a1a23383267cd8418bda03
tema-liu <vickyliu22205960@gmail.com>
2024-12-10 17:12:54 +0800

    新增-PrimaryBtn元件

---
+++ src/component/PrimaryBtn.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import { Icon } from "./LayoutComponents";
+
+type buttonStyleProps = {
+  $bgColor?: string;
+  $iconColor?: string;
+};
+
+type buttonProps = {
+  iconName?: string;
+  content: string;
+  $bgColor?: string;
+  $iconColor?: string;
+  onClick?: () => void; // 可選的點擊事件處理函式
+};
+
+const IconImg = styled(Icon)<buttonStyleProps>`
+  color: ${({ $iconColor, theme }) => $iconColor && theme.colors[$iconColor]};
+`;
+
+const Btn = styled.button<buttonStyleProps>`
+  width: 100%;
+  column-gap: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ $bgColor, theme }) =>
+    $bgColor ? theme.colors[$bgColor] : theme.colors.outline3};
+  border-radius: 16px;
+  padding: 13px 0;
+  box-shadow: 0px 0px 4px 0px #00000033, 0px 0px 8px 0px #0000001a;
+`;
+
+function PrimaryBtn({
+  iconName,
+  content,
+  $bgColor,
+  $iconColor,
+  onClick,
+}: buttonProps) {
+  return (
+    <Btn onClick={onClick} $bgColor={$bgColor}>
+      {iconName && (
+        <IconImg $iconColor={$iconColor} className="material-symbols-outlined">
+          {iconName}
+        </IconImg>
+      )}
+      {content}
+    </Btn>
+  );
+}
+
+export default PrimaryBtn;
