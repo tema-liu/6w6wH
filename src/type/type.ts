ee1865ffc519f759145b76c9272d1502e26ba75b
tema-liu <vickyliu22205960@gmail.com>
2024-12-15 23:50:02 +0800

    新增-商店詳細頁面型別

--- src/type/type.ts
+++ src/type/type.ts
@@ -46,3 +46,49 @@ export type hotReviewData = null | {
     latest: Comment;
   };
 };
+
+//storeDetail=============================================
+type Advertise = {
+  photo: string; // 廣告照片
+  url: string; // 廣告網址
+  title: string; // 廣告標題
+  slogan: string; // 廣告標語
+};
+
+type OpeningHours = {
+  Monday?: string | null;
+  Tuesday?: string | null;
+  Wednesday?: string | null;
+  Thursday?: string | null;
+  Friday?: string | null;
+  Saturday?: string | null;
+  Sunday?: string | null;
+};
+
+type Tag = {
+  tagName: string;
+  count: number;
+};
+
+type Location = {
+  lat: number;
+  lng: number;
+};
+
+export type StoreData = {
+  advertise?: Advertise | null; // 廣告資料
+  starCount?: number; // 星星數量
+  tags?: Tag[] | null; // 標籤列表
+  isFavorited: boolean; // 是否收藏
+  placeId: string; // Google 地點 ID
+  location: Location; // 經緯度
+  displayName: string; // 店名
+  photos?: string[] | null; // 店鋪照片
+  address?: string | null; // 地址
+  enAddress?: string | null; // 英文地址
+  Book?: string | null; // 預約網址
+  budget?: string | null; // 預算
+  phone?: string | null; // 電話
+  url?: string | null; // 網址
+  opening_hours?: OpeningHours | null; // 營業時間
+};
