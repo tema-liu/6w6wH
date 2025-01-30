6341578a9f58f3067c36af68b063f05294a3fef5
tema-liu <vickyliu22205960@gmail.com>
2025-01-31 01:05:42 +0800

    修正-MessageBox的按鈕 -增加邏輯判斷是否disabled -將原本的btn元件更改為通用按鈕PrimaryBtn

--- src/pages/Reviews/MessageBox.tsx
+++ src/pages/Reviews/MessageBox.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Icon } from "../../component/Layout/LayoutComponents";
 import { useState } from "react";
 import { CommentReply } from "../../type/formType";
 import { postCommentReply } from "../../apis/postCommentReply";
@@ -7,6 +6,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../utils/redux/store";
 import { Reply } from "../../type/type";
 import useAuthVerify from "../../hooks/useAuthVerify ";
+import { PrimaryBtn } from "../../component/Button/PrimaryBtn";
 
 const Box = styled.div`
   display: flex;
@@ -32,18 +32,6 @@ const Message = styled.input`
   background-color: ${({ theme }) => theme.colors.light};
 `;
 
-type ButtonProps = {
-  $bgColor?: string;
-};
-const Button = styled.button<ButtonProps>`
-  padding: 12px;
-  font-size: 0;
-  background-color: ${({ $bgColor, theme }) =>
-    $bgColor?.trim() !== "" ? theme.colors.outline3 : theme.colors.gray400};
-  box-shadow: 0px 0px 4px 0px #00000033, 0px 0px 8px 0px #0000001a;
-  border-radius: 8px;
-`;
-
 type moreProps = {
   userId: number;
   commentId: number;
@@ -52,6 +40,7 @@ type moreProps = {
 
 function MessageBox({ userId, commentId, onAddReply }: moreProps) {
   const [inputValue, setInputValue] = useState("");
+  const [isSubmit, setSubmit] = useState(false);
   const reply: CommentReply = {
     userId: userId,
     commentId: commentId,
@@ -60,6 +49,8 @@ function MessageBox({ userId, commentId, onAddReply }: moreProps) {
   const token = useSelector((state: RootState) => state.auth.token);
   const authVerify = useAuthVerify(token);
   const handleBtnClick = async () => {
+    console.log("123");
+    setSubmit(true);
     if (inputValue.trim()) {
       // 驗證是否登入
       const isAuthenticated = await authVerify();
@@ -75,6 +66,7 @@ function MessageBox({ userId, commentId, onAddReply }: moreProps) {
     } else {
       alert("Please do not input blank spaces.");
     }
+    setSubmit(false);
   };
 
   return (
@@ -84,12 +76,18 @@ function MessageBox({ userId, commentId, onAddReply }: moreProps) {
         placeholder="Reply to this review"
         onChange={(e) => setInputValue(e.target.value)}
       ></Message>
-
-      <Button $bgColor={inputValue} onClick={handleBtnClick}>
-        <Icon $isPointer={true} className="material-symbols-outlined">
-          add_comment
-        </Icon>
-      </Button>
+      <PrimaryBtn
+        disabled={isSubmit}
+        onClick={handleBtnClick}
+        $bgColor={
+          !isSubmit && inputValue.trim() !== "" ? "outline3" : "gray400"
+        }
+        iconName="add_comment"
+        $Width="fit-content"
+        $borderRadius={8}
+        $padding="12px"
+        $boxShadow=" 0px 0px 4px 0px #00000033, 0px 0px 8px 0px #0000001a"
+      />
     </Box>
   );
 }
