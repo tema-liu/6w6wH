b8a41f1537f62200c127eb59b757913d7d60b4ce
tema-liu <vickyliu22205960@gmail.com>
2025-02-02 18:01:22 +0800

    新增-更改語言功能完成 -可更改為印尼語及英語

--- src/pages/Settings/LangSelect.tsx
+++ src/pages/Settings/LangSelect.tsx
@@ -1,17 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import { languages } from "./data/langSelect";
 import { Select, SelectBox } from "./style/langSelect";
+import i18n from "../../utils/i18n/i18n";
 
 function LangSelect() {
+  const navigate = useNavigate();
+  const lang = localStorage.getItem("i18nextLng");
   const handleBlur = (event: React.FocusEvent<HTMLSelectElement>) => {
     // 取消焦点
     event.target.blur();
+    if (lang !== event.target.value) {
+      localStorage.setItem("i18nextLng", event.target.value); //重新預設local的語言
+      i18n.changeLanguage(event.target.value); //更改語言
+      navigate("/");
+    }
   };
 
   return (
     <SelectBox htmlFor="lang">
       <Select id="lang" onChange={handleBlur}>
         {languages.map(({ code, label }) => (
-          <option key={code} value={code}>
+          <option selected={lang === code} key={code} value={code}>
             {label}
           </option>
         ))}
